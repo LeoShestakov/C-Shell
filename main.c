1529b6e8989ddfbd339b82992e54d6f876cc9d28
athompson00 <athompson00@stuy.edu>
2019-12-15 17:57:36 -0500

    got multi cmds working

--- main.c
+++ main.c
@@ -8,7 +8,8 @@
 #include <unistd.h>
 #include <errno.h>
 
-char ** processInput(char *line);
+
+char *** processInput(char *line);
 
 int main() {
   while(1){
@@ -19,40 +20,53 @@ int main() {
     char input[100];
     fgets(input, 100, stdin);
     input[strlen(input) - 1] = NULL;
-    char ** newInput = processInput(input);
+    char *** newInput = processInput(input);
 
-
-    pid_t pid;
-    time_t t;
-    int status;
-    pid = fork();
-
-    if(!strcmp(newInput[0], "cd")){
-      chdir(newInput[1]);
-      pid = wait(&status);
-    }
-    else if(! strcmp(newInput[0], "exit")){
-        exit(0);
-    }
-    else if (pid > 0){
+    int x = 0;
+    while(newInput[x] != 0){
+      pid_t pid;
+      time_t t;
+      int status;
+      pid = fork();
+      if(!strcmp(newInput[x][0], "cd")){
+        chdir(newInput[x][1]);
         pid = wait(&status);
-    }
-    else if (pid == 0){
-        printf("NORMAL EXEC");
-        execvp(newInput[0], newInput);
+      }
+      else if(! strcmp(newInput[x][0], "exit")){
+          exit(0);
+      }
+      else if (pid > 0){
+          pid = wait(&status);
+      }
+      else if (pid == 0){
+          printf("NORMAL EXEC");
+          execvp(newInput[x][0], newInput[x]);
+      }
+      x++;
     }
   }
+ printf("test");
+  //processInput("hello buddy this is ; a test ; of our function");
 }
 
-char ** processInput(char *line){
+char *** processInput(char *line){
     int current = 0;
     int size = 0;
-    char** input = malloc(0);
+    char*** input = malloc(0);
+    int x = 0;
     while(line){
-      input = realloc(input, sizeof(char*)*current + sizeof(char*));
-      input[current] = strsep(&line, " ");
-      current++;
+      input[x] = realloc(input[x], sizeof(char*)*current + sizeof(char*));
+      input[x][current] = strsep(&line, " ");
+      printf("%s", input[x][current]);
+      if (!strcmp(input[x][current],";")){
+        input[x][current] = NULL;
+        x++;
+        current = 0;
+      } else {
+        current++;
+      }
     }
-    input[current] = 0;
+    input[x][current] = 0;
+    input[x + 1] = 0;
     return input;
 }
